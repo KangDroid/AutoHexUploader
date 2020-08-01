ae3395b1948c2af28e51492c421b5eb2b4f858b3
KangDroid <hyunwoo286@gmail.com>
2020-08-01 21:47:31 +0900

    Attach Logger on Timer

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -57,32 +57,44 @@ void Timer::set_schedule() {
     // Step 3. Set new date
     int size_queue = next_execution.size();
     int to_iterate = MAX_SCHEDULE - size_queue;
-    cout << shared_var->duration << endl;
+    LOG_V("Timer Queue Size: " + to_string(size_queue));
+    LOG_V("Timer Queue Iteration Count: " + to_string(to_iterate));
+    LOG_V("Timer Duration: " + shared_var->duration);
     if (shared_var->duration == "hour") {
         for (int i = 0; i < to_iterate; i++) {
             hour += shared_var->duration_number;
-            next_execution.push(dateToStamp(year, month, day, hour, min));
+            time_t stamp_using = dateToStamp(year, month, day, hour, min);
+            LOG_V("Next Execution[Epoch]: " + to_string(stamp_using));
+            next_execution.push(stamp_using);
         }
     } else if (shared_var->duration == "month") {
         for (int i = 0; i < to_iterate; i++) {
             month += shared_var->duration_number;
-            next_execution.push(dateToStamp(year, month, day, hour, min));
+            time_t stamp_using = dateToStamp(year, month, day, hour, min);
+            LOG_V("Next Execution[Epoch]: " + to_string(stamp_using));
+            next_execution.push(stamp_using);
         }
     } else if (shared_var->duration == "week") {
         for (int i = 0; i < to_iterate; i++) {
             day += 7 * shared_var->duration_number;
-            next_execution.push(dateToStamp(year, month, day, hour, min));
+            time_t stamp_using = dateToStamp(year, month, day, hour, min);
+            LOG_V("Next Execution[Epoch]: " + to_string(stamp_using));
+            next_execution.push(stamp_using);
         }
         cout << "Week specified" << endl;
     } else if (shared_var->duration == "day") {
         for (int i = 0; i < to_iterate; i++) {
             day += shared_var->duration_number;
-            next_execution.push(dateToStamp(year, month, day, hour, min));
+            time_t stamp_using = dateToStamp(year, month, day, hour, min);
+            LOG_V("Next Execution[Epoch]: " + to_string(stamp_using));
+            next_execution.push(stamp_using);
         }
     } else if (shared_var->duration == "minute") {
         for (int i = 0; i < to_iterate; i++) {
             min += shared_var->duration_number;
-            next_execution.push(dateToStamp(year, month, day, hour, min));
+            time_t stamp_using = dateToStamp(year, month, day, hour, min);
+            LOG_V("Next Execution[Epoch]: " + to_string(stamp_using));
+            next_execution.push(stamp_using);
         }
     }
 }
@@ -97,6 +109,5 @@ void Timer::show_schedule() {
 Timer::Timer(BasicVariableInfo* v) {
     this->shared_var = v;
     get_current_time_var(this->year_ref, this->month_ref, this->day_ref, this->hour_ref, this->min_ref);
-    // this->duration_integer = shared_var->duration_number;
-    // this->duration_string = shared_var->duration;
+    LOG_V("Reference Time[Timer First Call]: " + to_string(year_ref)+ "-" + to_string(month_ref) + "-" + to_string(day_ref) + ", at: " + to_string(hour_ref) + ":" + to_string(min_ref));
 }
