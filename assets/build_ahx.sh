efdc1326d3b2a04b54cce65a8ee1b52095922ddf
KangDroid <hyunwoo286@gmail.com>
2020-08-03 19:11:32 +0900

    Add self-building scripts for both rpi/osx

---
+++ assets/build_ahx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# path
+export MAIN_WORKING_DIR=/tmp/ahx
+
+# clone this repo
+git clone https://github.com/KangDroid/AutoHexUploader $MAIN_WORKING_DIR
+
+cd $MAIN_WORKING_DIR
+mkdir build
+cd build
+cmake ..
+make -j12
+make -j2 install
